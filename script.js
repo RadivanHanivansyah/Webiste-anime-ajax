bcf65cacf04176f7fd5fb6380bf6c8b4131589ad
RadivanHanivansyah <radivan100@gmail.com>
2025-01-27 17:50:33 +0700

    Membuat function utk menampilkan di web (sampai class Synopsis(blm fix))

--- script.js
+++ script.js
@@ -12,13 +12,22 @@ function getData(url) {
 
 processData = (data) => {
     const title = [];
+    const titleEnglish = [];
     const dateAiring = [];
     const totalEpisode = [];
     const genre = [];
-    console.log(data.data[0]);
+    const sinopsis = [];
+    const studio = [];
+    const source = [];
+    const theme = [];
+    const img = [];
+    const type = [];
+    const score = [];
+    const member = [];
     data.data.forEach((array, index) => {
         //Memasukkan data ke array title
         title.push(array.title);
+        titleEnglish.push(array.title_english);
 
         //Memasukkan data ke array dateAiring
         const date = array.aired.string;
@@ -26,12 +35,70 @@ processData = (data) => {
         dateAiring[index] = dateAiring[index].replace("to ?", "");
 
         //Memasukkan data ke array totalEpisode
-        const episode = array.episodes + ", " + array.duration;
+        const episode = array.episodes + " eps" + ", " + array.duration;
         totalEpisode.push(episode);
+        const episodeType = array.type + "(" + array.episodes + ")";
+        type.push(episodeType);
 
-        genre.push(array.genres)
+
+        // Memasukkan data ke array genre
+        genre.push(array.genres);
+
+        // Memasukkan data ke array sinposis
+        sinopsis.push(array.synopsis);
+
+        // Memasukkan data ke array studio
+        studio.push(array.studios[0].name);
+
+        // Memasukkan data ke array source
+        source.push(array.source)
+
+        // Memasukkan data ke array theme
+        theme.push(array.themes);
+
+        // Memasukkan data ke array img
+        img.push(array.images.jpg.image_url);
+
+        // Memasukkan data ke array img
+        score.push(array.score);
+
+        // Memasukkan data ke array member
+        member.push(array.members);
+        member[index] = member[index].toString();
+        member[index] = member[index].slice(0, 3) + "k";
+
     });
-    console.log(genre);
+    showData(title, titleEnglish, dateAiring, totalEpisode, genre,
+        type, sinopsis, studio, source, theme, img, score, member);
+
+}
+
+showData = (title, titleEnglish, dateAiring, totalEpisode, genre,
+    type, sinopsis, studio, source, theme, img, score, member) => {
+    const titleJapan = document.querySelectorAll('.title.japan');
+    const englishTitle = document.querySelectorAll('.title.english');
+    const image = document.querySelectorAll('#image');
+    const airing = document.querySelectorAll('.date');
+    const episode = document.querySelectorAll('.total.episode');
+    const synopsis = document.querySelectorAll('.Synopsis');
+    titleJapan.forEach((element, index) => {
+        element.textContent = title[index];
+    })
+    englishTitle.forEach((element, index) => {
+        element.textContent = titleEnglish[index];
+    })
+    image.forEach((element, index) => {
+        element.src = img[index];
+    })
+    airing.forEach((element, index) => {
+        element.textContent = dateAiring[index];
+    })
+    episode.forEach((element, index) => {
+        element.textContent = totalEpisode[index];
+    })
+    synopsis.forEach((element, index) => {
+        element.textContent = sinopsis[index];
+    })
 }
 
 getData(url);
